9029a802f673951ce57418a650a236b15c4df132
Oscary13 <oscary_1320114020@uptecamac.edu.mx>
2023-11-25 22:44:06 -0600

    25/11/2023 -b

--- GenerarPDF/GenerarPDF/obj/Debug/netstandard2.0/GenerarPDF.AssemblyInfo.cs
+++ GenerarPDF/GenerarPDF/obj/Debug/netstandard2.0/GenerarPDF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GenerarPDF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6132d42fe1cd68692ca39095f8d22d38139335e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GenerarPDF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GenerarPDF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
